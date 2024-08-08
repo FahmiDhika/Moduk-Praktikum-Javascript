5efde89d0f31bd04eaf92d036ec734f4e5e01eb3
FahmiDhika <fahmidhika810@gmail.com>
2024-08-08 14:23:42 +0700

    Tugas Input Melalui HTML

--- tugas3.js
+++ tugas3.js
@@ -1,16 +1,21 @@
-console.log("Soal No.3")
-let tb = 170
-const tbafter = tb / 100
-let bb = 90
-const bmi = bb / (tbafter * tbafter)
-console.log("Hasil BMI : " + bmi.toLocaleString())
+function calculateBMI() {
+  const button = document.getElementById("btn")
+  const tb = Number(document.getElementById("tb").value)
+  const bb = Number(document.getElementById("bb").value)
 
-if (bmi <= 18.5) {
-  console.log("Keterangan : Stunting")
-} else if (bmi <= 24.9) {
-  console.log("Keterangan : Normal")
-} else if (bmi <= 29.9) {
-  console.log("Keterangan : Kelebihan Berat Badan")
-} else {
-  console.log("Keterangan : Obesitas")
+  const tbafter = tb / 100
+
+  const bmi = bb / (tbafter * tbafter)
+
+  document.getElementById("hasil").innerHTML = "Hasil BMI : " + bmi.toFixed(2)
+
+  if (bmi <= 18.5) {
+    document.getElementById("keterangan").innerHTML = "Keterangan : Stunting"
+  } else if (bmi <= 24.9) {
+    document.getElementById("keterangan").innerHTML = "Keterangan : Normal"
+  } else if (bmi <= 29.9) {
+    document.getElementById("keterangan").innerHTML = "Keterangan : Kelebihan Berat Badan"
+  } else {
+    document.getElementById("keterangan").innerHTML = "Keterangan : Obesitas"
+  }
 }
